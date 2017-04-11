8a500c93f282af7040c3187d5b5d4c0a0845edd8
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    isig c2

--- kilo.c
+++ kilo.c
@@ -15,7 +15,7 @@ void enableRawMode() {
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
