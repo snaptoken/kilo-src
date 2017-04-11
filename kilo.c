cd133666743b50ade165f25fecec28628cdaafe0
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    tildes c2

--- kilo.c
+++ kilo.c
@@ -56,8 +56,19 @@ char editorReadKey() {
 
 /*** output ***/
 
+void editorDrawRows() {
+  int y;
+  for (y = 0; y < 24; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
 void editorRefreshScreen() {
   write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  editorDrawRows();
+
   write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
