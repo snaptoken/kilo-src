5070931545ec953d45df6af51a0aeed8ae8500f8
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    increment-dirty c2

--- kilo.c
+++ kilo.c
@@ -229,6 +229,7 @@ void editorAppendRow(char *s, size_t len) {
   editorUpdateRow(&E.row[at]);
 
   E.numrows++;
+  E.dirty++;
 }
 
 void editorRowInsertChar(erow *row, int at, int c) {
@@ -238,6 +239,7 @@ void editorRowInsertChar(erow *row, int at, int c) {
   row->size++;
   row->chars[at] = c;
   editorUpdateRow(row);
+  E.dirty++;
 }
 
 /*** editor operations ***/
