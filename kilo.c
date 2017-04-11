7650a4b609f4ba69c21e71a9e074b68a5c963522
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    editor-update-syntax c1

--- kilo.c
+++ kilo.c
@@ -189,6 +189,20 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+/*** syntax highlighting ***/
+
+void editorUpdateSyntax(erow *row) {
+  row->hl = realloc(row->hl, row->rsize);
+  memset(row->hl, HL_NORMAL, row->rsize);
+
+  int i;
+  for (i = 0; i < row->rsize; i++) {
+    if (isdigit(row->render[i])) {
+      row->hl[i] = HL_NUMBER;
+    }
+  }
+}
+
 /*** row operations ***/
 
 int editorRowCxToRx(erow *row, int cx) {
