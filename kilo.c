9483b45b38b16ad24241618f1cc2335d05ccff62
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    hl-comment c1

--- kilo.c
+++ kilo.c
@@ -40,6 +40,7 @@ enum editorKey {
 
 enum editorHighlight {
   HL_NORMAL = 0,
+  HL_COMMENT,
   HL_STRING,
   HL_NUMBER,
   HL_MATCH
@@ -274,6 +275,7 @@ void editorUpdateSyntax(erow *row) {
 
 int editorSyntaxToColor(int hl) {
   switch (hl) {
+    case HL_COMMENT: return 36;
     case HL_STRING: return 35;
     case HL_NUMBER: return 31;
     case HL_MATCH: return 34;
