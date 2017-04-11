6022ee9911fa1f067fda9cf97f0573f4c503fb83
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    cx-cy c1

--- kilo.c
+++ kilo.c
@@ -18,6 +18,7 @@
 /*** data ***/
 
 struct editorConfig {
+  int cx, cy;
   int screenrows;
   int screencols;
   struct termios orig_termios;
@@ -178,6 +179,9 @@ void editorProcessKeypress() {
 /*** init ***/
 
 void initEditor() {
+  E.cx = 0;
+  E.cy = 0;
+
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
