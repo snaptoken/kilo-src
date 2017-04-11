9694960ac3b06319fb5c93a172506bd472380a7f
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    last-line c2

--- kilo.c
+++ kilo.c
@@ -98,7 +98,11 @@ int getWindowSize(int *rows, int *cols) {
 void editorDrawRows() {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
+    write(STDOUT_FILENO, "~", 1);
+
+    if (y < E.screenrows - 1) {
+      write(STDOUT_FILENO, "\r\n", 2);
+    }
   }
 }
 
