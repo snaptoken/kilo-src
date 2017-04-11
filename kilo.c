ff6c88ef0655da86fa4a5928658b54ffad9c4290
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    end-key c2

--- kilo.c
+++ kilo.c
@@ -393,7 +393,8 @@ void editorProcessKeypress() {
       break;
 
     case END_KEY:
-      E.cx = E.screencols - 1;
+      if (E.cy < E.numrows)
+        E.cx = E.row[E.cy].size;
       break;
 
     case PAGE_UP:
