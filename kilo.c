8f9fc4e52566d4e9f68f1a14e0c59babaf642f92
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    editor-syntax c1

--- kilo.c
+++ kilo.c
@@ -44,7 +44,15 @@ enum editorHighlight {
   HL_MATCH
 };
 
+#define HL_HIGHLIGHT_NUMBERS (1<<0)
+
 /*** data ***/
+
+struct editorSyntax {
+  char *filetype;
+  char **filematch;
+  int flags;
+};
 
 typedef struct erow {
   int size;
