666773dcd8986076ed352b501166f7ba9c586c8d
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    back-to-ioctl c1

--- kilo.c
+++ kilo.c
@@ -83,7 +83,7 @@ int getCursorPosition(int *rows, int *cols) {
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
 
-  if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
     if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
     return getCursorPosition(rows, cols);
   } else {
