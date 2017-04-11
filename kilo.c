bbfbd0820291f4220bb02173c8856b0b56a5add1
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    atexit c2

--- kilo.c
+++ kilo.c
@@ -1,11 +1,18 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode() {
-  struct termios raw;
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
 
-  tcgetattr(STDIN_FILENO, &raw);
-
+  struct termios raw = orig_termios;
   raw.c_lflag &= ~(ECHO);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
