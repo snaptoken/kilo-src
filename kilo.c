e28604bf8aee076cb6ece8d1f244dc462102565a
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    scs c1

--- kilo.c
+++ kilo.c
@@ -54,6 +54,7 @@ enum editorHighlight {
 struct editorSyntax {
   char *filetype;
   char **filematch;
+  char *singleline_comment_start;
   int flags;
 };
 
@@ -92,6 +93,7 @@ struct editorSyntax HLDB[] = {
   {
     "c",
     C_HL_extensions,
+    "//",
     HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
   },
 };
