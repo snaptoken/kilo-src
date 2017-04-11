4a3362703d3dbef569da3176dd9ffcb556c8c0eb
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    insert-newline c1

--- kilo.c
+++ kilo.c
@@ -285,6 +285,21 @@ void editorInsertChar(int c) {
   E.cx++;
 }
 
+void editorInsertNewline() {
+  if (E.cx == 0) {
+    editorInsertRow(E.cy, "", 0);
+  } else {
+    erow *row = &E.row[E.cy];
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+    row = &E.row[E.cy];
+    row->size = E.cx;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+  }
+  E.cy++;
+  E.cx = 0;
+}
+
 void editorDelChar() {
   if (E.cy == E.numrows) return;
   if (E.cx == 0 && E.cy == 0) return;
