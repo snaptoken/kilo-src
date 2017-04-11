465bced1320edcd94800e25a6c9bd0f7598c4ded
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    incremental-search c2

--- kilo.c
+++ kilo.c
@@ -405,9 +405,10 @@ void editorSave() {
 
 /*** find ***/
 
-void editorFind() {
-  char *query = editorPrompt("Search: %s (ESC to cancel)", NULL);
-  if (query == NULL) return;
+void editorFindCallback(char *query, int key) {
+  if (key == '\r' || key == '\x1b') {
+    return;
+  }
 
   int i;
   for (i = 0; i < E.numrows; i++) {
@@ -420,8 +421,14 @@ void editorFind() {
       break;
     }
   }
+}
 
-  free(query);
+void editorFind() {
+  char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+
+  if (query) {
+    free(query);
+  }
 }
 
 /*** append buffer ***/
