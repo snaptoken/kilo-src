394338f430559d89b3ce14566f054f2ae758e65d
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    bottom-right c2

--- kilo.c
+++ kilo.c
@@ -64,7 +64,9 @@ char editorReadKey() {
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
 
-  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+  if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+    editorReadKey();
     return -1;
   } else {
     *cols = ws.ws_col;
