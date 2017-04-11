8c2388681d92cc0ae5ca58786e9fa14f37a31b0a
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    fix-append-row c0

--- kilo.c
+++ kilo.c
@@ -165,11 +165,14 @@ int getWindowSize(int *rows, int *cols) {
 /*** row operations ***/
 
 void editorAppendRow(char *s, size_t len) {
-  E.row.size = len;
-  E.row.chars = malloc(len + 1);
-  memcpy(E.row.chars, s, len);
-  E.row.chars[len] = '\0';
-  E.numrows = 1;
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+
+  int at = E.numrows;
+  E.row[at].size = len;
+  E.row[at].chars = malloc(len + 1);
+  memcpy(E.row[at].chars, s, len);
+  E.row[at].chars[len] = '\0';
+  E.numrows++;
 }
 
 /*** file i/o ***/
