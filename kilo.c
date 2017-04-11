f343c737c09488b31b2884cfc1cb236064bd2cb1
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    abuf-append c1

--- kilo.c
+++ kilo.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -101,6 +102,19 @@ struct abuf {
 };
 
 #define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+  char *new = realloc(ab->b, ab->len + len);
+
+  if (new == NULL) return;
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+  free(ab->b);
+}
 
 /*** output ***/
 
