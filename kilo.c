d5175be767aba7e9aaa01b6dc8d329ef105df0db
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    cx-to-rx c1

--- kilo.c
+++ kilo.c
@@ -170,6 +170,17 @@ int getWindowSize(int *rows, int *cols) {
 
 /*** row operations ***/
 
+int editorRowCxToRx(erow *row, int cx) {
+  int rx = 0;
+  int j;
+  for (j = 0; j < cx; j++) {
+    if (row->chars[j] == '\t')
+      rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
+    rx++;
+  }
+  return rx;
+}
+
 void editorUpdateRow(erow *row) {
   int tabs = 0;
   int j;
