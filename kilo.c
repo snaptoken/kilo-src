722ec705da177082575df642f6d810dc6affb429
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    editor-scroll-horizontal c1

--- kilo.c
+++ kilo.c
@@ -227,6 +227,12 @@ void editorScroll() {
   if (E.cy >= E.rowoff + E.screenrows) {
     E.rowoff = E.cy - E.screenrows + 1;
   }
+  if (E.cx < E.coloff) {
+    E.coloff = E.cx;
+  }
+  if (E.cx >= E.coloff + E.screencols) {
+    E.coloff = E.cx - E.screencols + 1;
+  }
 }
 
 void editorDrawRows(struct abuf *ab) {
