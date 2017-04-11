36c691e869f531755f857d57f1349151be6debc0
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:50 -0600

    mcs-mce-len c1

--- kilo.c
+++ kilo.c
@@ -247,7 +247,12 @@ void editorUpdateSyntax(erow *row) {
   char **keywords = E.syntax->keywords;
 
   char *scs = E.syntax->singleline_comment_start;
+  char *mcs = E.syntax->multiline_comment_start;
+  char *mce = E.syntax->multiline_comment_end;
+
   int scs_len = scs ? strlen(scs) : 0;
+  int mcs_len = mcs ? strlen(mcs) : 0;
+  int mce_len = mce ? strlen(mce) : 0;
 
   int prev_sep = 1;
   int in_string = 0;
