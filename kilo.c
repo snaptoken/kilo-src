39a90fb7207c5f68eabddd800118bf7c9eceffba
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    coloff c1

--- kilo.c
+++ kilo.c
@@ -42,6 +42,7 @@ typedef struct erow {
 struct editorConfig {
   int cx, cy;
   int rowoff;
+  int coloff;
   int screenrows;
   int screencols;
   int numrows;
@@ -350,6 +351,7 @@ void initEditor() {
   E.cx = 0;
   E.cy = 0;
   E.rowoff = 0;
+  E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
 
