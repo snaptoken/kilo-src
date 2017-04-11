117d5e8b47b2623aaa13d7c490ded021cf6ba335
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    echo c2

--- kilo.c
+++ kilo.c
@@ -1,6 +1,19 @@
+#include <termios.h>
 #include <unistd.h>
 
+void enableRawMode() {
+  struct termios raw;
+
+  tcgetattr(STDIN_FILENO, &raw);
+
+  raw.c_lflag &= ~(ECHO);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
+  enableRawMode();
+
   char c;
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
   return 0;
