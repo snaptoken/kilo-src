b365f521b650d5147e64d6d8225737c585194bbf
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    detect-delete-key c1

--- kilo.c
+++ kilo.c
@@ -20,6 +20,7 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  DEL_KEY,
   HOME_KEY,
   END_KEY,
   PAGE_UP,
@@ -86,6 +87,7 @@ int editorReadKey() {
         if (seq[2] == '~') {
           switch (seq[1]) {
             case '1': return HOME_KEY;
+            case '3': return DEL_KEY;
             case '4': return END_KEY;
             case '5': return PAGE_UP;
             case '6': return PAGE_DOWN;
