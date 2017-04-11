92c8f6ae1f4ce08d46146f697c62e41a69ea0d47
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:50 -0600

    mcs-mce c1

--- kilo.c
+++ kilo.c
@@ -59,6 +59,8 @@ struct editorSyntax {
   char **filematch;
   char **keywords;
   char *singleline_comment_start;
+  char *multiline_comment_start;
+  char *multiline_comment_end;
   int flags;
 };
 
@@ -105,7 +107,7 @@ struct editorSyntax HLDB[] = {
     "c",
     C_HL_extensions,
     C_HL_keywords,
-    "//",
+    "//", "/*", "*/",
     HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
   },
 };
