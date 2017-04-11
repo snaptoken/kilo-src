749253efef7df54b9efecdfff4794d479e709ab5
snaptoken <snaptoken@viewsourcecode.org>
2017-04-10 21:43:49 -0600

    page-up-down c2

--- kilo.c
+++ kilo.c
@@ -399,6 +399,13 @@ void editorProcessKeypress() {
     case PAGE_UP:
     case PAGE_DOWN:
       {
+        if (c == PAGE_UP) {
+          E.cy = E.rowoff;
+        } else if (c == PAGE_DOWN) {
+          E.cy = E.rowoff + E.screenrows - 1;
+          if (E.cy > E.numrows) E.cy = E.numrows;
+        }
+
         int times = E.screenrows;
         while (times--)
           editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
