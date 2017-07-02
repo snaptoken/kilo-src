3e4933b004d3eacdbc516624f4de9da5e78e2839
snaptoken <snaptoken@viewsourcecode.org>
2017-07-01 22:36:14 -0600

    render c1

--- kilo.c
+++ kilo.c
@@ -36,7 +36,9 @@ enum editorKey {
 
 typedef struct erow {
   int size;
+  int rsize;
   char *chars;
+  char *render;
 } erow;
 
 struct editorConfig {
@@ -174,6 +176,10 @@ void editorAppendRow(char *s, size_t len) {
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
   E.row[at].chars[len] = '\0';
+
+  E.row[at].rsize = 0;
+  E.row[at].render = NULL;
+
   E.numrows++;
 }
 
