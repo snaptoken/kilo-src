bc5da92c93d5667daf902accb8909051c9ada7ef
snaptoken <snaptoken@viewsourcecode.org>
2017-07-01 22:36:14 -0600

    prev-sep c2

--- kilo.c
+++ kilo.c
@@ -200,14 +200,21 @@ void editorUpdateSyntax(erow *row) {
   row->hl = realloc(row->hl, row->rsize);
   memset(row->hl, HL_NORMAL, row->rsize);
 
+  int prev_sep = 1;
+
   int i = 0;
   while (i < row->rsize) {
     char c = row->render[i];
+    unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
 
-    if (isdigit(c)) {
+    if (isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) {
       row->hl[i] = HL_NUMBER;
+      i++;
+      prev_sep = 0;
+      continue;
     }
 
+    prev_sep = is_separator(c);
     i++;
   }
 }
