8a2271840b56ba5b61f60ca3c48b8ddd72ae5fd7
snaptoken <snaptoken@viewsourcecode.org>
2017-07-01 22:36:14 -0600

    use-rx c1

--- kilo.c
+++ kilo.c
@@ -314,7 +314,7 @@ void editorRefreshScreen() {
 
   char buf[32];
   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
-                                            (E.cx - E.coloff) + 1);
+                                            (E.rx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6);
