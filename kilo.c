efc05d422bbd1f0e1e7b66fede90aff62724e535
snaptoken <snaptoken@viewsourcecode.org>
2017-07-01 22:36:14 -0600

    message-bar-make-room c2

--- kilo.c
+++ kilo.c
@@ -341,6 +341,7 @@ void editorDrawStatusBar(struct abuf *ab) {
     }
   }
   abAppend(ab, "\x1b[m", 3);
+  abAppend(ab, "\r\n", 2);
 }
 
 void editorRefreshScreen() {
@@ -473,7 +474,7 @@ void initEditor() {
   E.statusmsg_time = 0;
 
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
-  E.screenrows -= 1;
+  E.screenrows -= 2;
 }
 
 int main(int argc, char *argv[]) {
