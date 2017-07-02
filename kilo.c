70b016b7a949a3a2cf325cb50e31e51564d1ee7f
snaptoken <snaptoken@viewsourcecode.org>
2017-07-01 22:36:13 -0600

    abuf-struct c1

--- kilo.c
+++ kilo.c
@@ -93,6 +93,15 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+/*** append buffer ***/
+
+struct abuf {
+  char *b;
+  int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
 /*** output ***/
 
 void editorDrawRows() {
