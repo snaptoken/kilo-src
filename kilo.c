4b989815dd346b8aae1f8d0f215884a955b628f8
snaptoken <snaptoken@viewsourcecode.org>
2017-07-01 22:36:14 -0600

    callback-statics c1

--- kilo.c
+++ kilo.c
@@ -406,8 +406,20 @@ void editorSave() {
 /*** find ***/
 
 void editorFindCallback(char *query, int key) {
+  static int last_match = -1;
+  static int direction = 1;
+
   if (key == '\r' || key == '\x1b') {
+    last_match = -1;
+    direction = 1;
     return;
+  } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+    direction = 1;
+  } else if (key == ARROW_LEFT || key == ARROW_UP) {
+    direction = -1;
+  } else {
+    last_match = -1;
+    direction = 1;
   }
 
   int i;
