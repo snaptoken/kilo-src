ac249dcc7c0d0ec5b333502c4c9250241703e5aa
snaptoken <snaptoken@viewsourcecode.org>
2017-07-01 22:36:14 -0600

    rx-to-cx c1

--- kilo.c
+++ kilo.c
@@ -196,6 +196,19 @@ int editorRowCxToRx(erow *row, int cx) {
   return rx;
 }
 
+int editorRowRxToCx(erow *row, int rx) {
+  int cur_rx = 0;
+  int cx;
+  for (cx = 0; cx < row->size; cx++) {
+    if (row->chars[cx] == '\t')
+      cur_rx += (KILO_TAB_STOP - 1) - (cur_rx % KILO_TAB_STOP);
+    cur_rx++;
+
+    if (cur_rx > rx) return cx;
+  }
+  return cx;
+}
+
 void editorUpdateRow(erow *row) {
   int tabs = 0;
   int j;
