c5635855f3c5c82c95abc0c51768a7b31efd7f67
snaptoken <snaptoken@viewsourcecode.org>
2017-07-01 22:36:13 -0600

    erow c1

--- kilo.c
+++ kilo.c
@@ -29,10 +29,17 @@ enum editorKey {
 
 /*** data ***/
 
+typedef struct erow {
+  int size;
+  char *chars;
+} erow;
+
 struct editorConfig {
   int cx, cy;
   int screenrows;
   int screencols;
+  int numrows;
+  erow row;
   struct termios orig_termios;
 };
 
@@ -286,6 +293,7 @@ void editorProcessKeypress() {
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.numrows = 0;
 
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
