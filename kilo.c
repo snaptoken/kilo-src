c6004d7d93c8a045f401fb8bab2de8d7c4ebca81
snaptoken <snaptoken@viewsourcecode.org>
2017-07-01 22:36:13 -0600

    hide-cursor c1

--- kilo.c
+++ kilo.c
@@ -132,12 +132,14 @@ void editorDrawRows(struct abuf *ab) {
 void editorRefreshScreen() {
   struct abuf ab = ABUF_INIT;
 
+  abAppend(&ab, "\x1b[?25l", 6);
   abAppend(&ab, "\x1b[2J", 4);
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
 
   abAppend(&ab, "\x1b[H", 3);
+  abAppend(&ab, "\x1b[?25h", 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
