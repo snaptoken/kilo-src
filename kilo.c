300a0d4dcc79cf8a4fc7d632f2ae45d493589acf
snaptoken <snaptoken@viewsourcecode.org>
2017-07-01 22:36:14 -0600

    hl-multiline-comments c1

--- kilo.c
+++ kilo.c
@@ -41,6 +41,7 @@ enum editorKey {
 enum editorHighlight {
   HL_NORMAL = 0,
   HL_COMMENT,
+  HL_MLCOMMENT,
   HL_KEYWORD1,
   HL_KEYWORD2,
   HL_STRING,
@@ -320,7 +321,8 @@ void editorUpdateSyntax(erow *row) {
 
 int editorSyntaxToColor(int hl) {
   switch (hl) {
-    case HL_COMMENT: return 36;
+    case HL_COMMENT:
+    case HL_MLCOMMENT: return 36;
     case HL_KEYWORD1: return 33;
     case HL_KEYWORD2: return 32;
     case HL_STRING: return 35;
