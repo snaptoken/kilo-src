c6974011de0d2ef755abd4d73b2af169d7e90010
snaptoken <snaptoken@viewsourcecode.org>
2017-07-01 22:36:14 -0600

    editor-del-char c1

--- kilo.c
+++ kilo.c
@@ -261,6 +261,16 @@ void editorInsertChar(int c) {
   E.cx++;
 }
 
+void editorDelChar() {
+  if (E.cy == E.numrows) return;
+
+  erow *row = &E.row[E.cy];
+  if (E.cx > 0) {
+    editorRowDelChar(row, E.cx - 1);
+    E.cx--;
+  }
+}
+
 /*** file i/o ***/
 
 char *editorRowsToString(int *buflen) {
