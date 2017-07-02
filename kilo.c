8f7b66c1b0218c58aa1f194deb6c743e5a72f8bc
snaptoken <snaptoken@viewsourcecode.org>
2017-07-01 22:36:14 -0600

    syntax-while c1

--- kilo.c
+++ kilo.c
@@ -196,11 +196,15 @@ void editorUpdateSyntax(erow *row) {
   row->hl = realloc(row->hl, row->rsize);
   memset(row->hl, HL_NORMAL, row->rsize);
 
-  int i;
-  for (i = 0; i < row->rsize; i++) {
-    if (isdigit(row->render[i])) {
+  int i = 0;
+  while (i < row->rsize) {
+    char c = row->render[i];
+
+    if (isdigit(c)) {
       row->hl[i] = HL_NUMBER;
     }
+
+    i++;
   }
 }
 
