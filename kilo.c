dd93374d406e00047ae73d2da1e3871df647926d
snaptoken <snaptoken@viewsourcecode.org>
2017-07-01 22:36:13 -0600

    set-cursor-position c1

--- kilo.c
+++ kilo.c
@@ -155,7 +155,10 @@ void editorRefreshScreen() {
 
   editorDrawRows(&ab);
 
-  abAppend(&ab, "\x1b[H", 3);
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+
   abAppend(&ab, "\x1b[?25h", 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
