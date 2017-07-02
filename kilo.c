8576e2c56cba5b2b4bd91d14b0b32b5a305db7a3
snaptoken <snaptoken@viewsourcecode.org>
2017-07-01 22:36:13 -0600

    carriage-returns c2

--- kilo.c
+++ kilo.c
@@ -28,9 +28,9 @@ int main() {
   char c;
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
     if (iscntrl(c)) {
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     } else {
-      printf("%d ('%c')\n", c, c);
+      printf("%d ('%c')\r\n", c, c);
     }
   }
 
