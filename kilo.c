55d61baeb9b9bb72a17265e215106b653c629d2c
snaptoken <snaptoken@viewsourcecode.org>
2017-07-01 22:36:13 -0600

    init-editor c1

--- kilo.c
+++ kilo.c
@@ -15,6 +15,8 @@
 /*** data ***/
 
 struct editorConfig {
+  int screenrows;
+  int screencols;
   struct termios orig_termios;
 };
 
@@ -105,8 +107,13 @@ void editorProcessKeypress() {
 
 /*** init ***/
 
+void initEditor() {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main() {
   enableRawMode();
+  initEditor();
 
   while (1) {
     editorRefreshScreen();
