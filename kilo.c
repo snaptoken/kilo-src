eec647f67d72d6632012dbd6a028f78c30fe2600
snaptoken <snaptoken@viewsourcecode.org>
2017-07-01 22:36:13 -0600

    misc-flags c1

--- kilo.c
+++ kilo.c
@@ -15,8 +15,9 @@ void enableRawMode() {
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-  raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
