a7441eb2241d1719cb56c718f58ab627ad1dce57
snaptoken <snaptoken@viewsourcecode.org>
2017-07-01 22:36:14 -0600

    hldb c1

--- kilo.c
+++ kilo.c
@@ -80,6 +80,20 @@ struct editorConfig {
 
 struct editorConfig E;
 
+/*** filetypes ***/
+
+char *C_HL_extensions[] = { ".c", ".h", ".cpp", NULL };
+
+struct editorSyntax HLDB[] = {
+  {
+    "c",
+    C_HL_extensions,
+    HL_HIGHLIGHT_NUMBERS
+  },
+};
+
+#define HLDB_ENTRIES (sizeof(HLDB) / sizeof(HLDB[0]))
+
 /*** prototypes ***/
 
 void editorSetStatusMessage(const char *fmt, ...);
