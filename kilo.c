1f772538f3fb601c2cc9e556c94c96b35ec38d70
snaptoken <snaptoken@viewsourcecode.org>
2017-07-01 22:36:14 -0600

    snap-cursor c2

--- kilo.c
+++ kilo.c
@@ -317,6 +317,12 @@ void editorMoveCursor(int key) {
       }
       break;
   }
+
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if (E.cx > rowlen) {
+    E.cx = rowlen;
+  }
 }
 
 void editorProcessKeypress() {
